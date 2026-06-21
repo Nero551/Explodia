42ae4fe09a067cf869ce2219b97c8d7bcf9dca9e
Nero <109078102+Nero551@users.noreply.github.com>
2026-06-21 13:24:39 +0300

    testing ssh key

--- Main/Shared/Entities/Player.cs
+++ Main/Shared/Entities/Player.cs
@@ -6,12 +6,14 @@ namespace Entities;
 
 public class Player : Entity
 {
+    
     protected override void Initialize()
     {
+
         AddBlock<Blocks.TransformBlock>();
         AddBlock<Blocks.MovementBlock>();
         AddBlock<Blocks.InputBlock>();
-
+        
         if (NetworkService.IsClient())
         {
             Game.World.Players.AddChild(ConnectTo(SceneService.CreateScene<Node>("Shared/Scenes/Player")));
